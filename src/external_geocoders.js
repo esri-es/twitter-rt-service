ed7fb2cc1d0b202a7d30c12469cf9a31f609448d
Jorge Barrachina <nusquema@gmail.com>
2019-05-14 16:52:53 +0200

    Refactored normalize. Look up comments TO BE IMPLEMENTED for the tasks to be done. Moved Tips & examples to external_geocoders.js as a reference to look at

--- src/external_geocoders.js
+++ src/external_geocoders.js
@@ -1,3 +1,41 @@
+/*  ARCGIS_SAMPLE
+    Sample: https://cloudlab.esri.es/server/rest/services/ESP_AdminPlaces/GeocodeServer/findAddressCandidates?SingleLineCityName=nerja&outFields=*&f=pjson
+
+    "Nbrhd": "",
+    "City": "Nerja", --> municipio / ciudad
+    "Subregion": "Málaga", --> provincia
+    "Region": "Andalucía", --> ccaa
+    "Addr_type": "SubAdmin",
+    "Country": "ESP"
+*/
+
+/*        
+ Check both requirements:
+ - osm_type (by priority): ways | relation (if not -> node)
+ - class: boundary, administrative, postcode
+
+ TODO: async call to: https://nominatim.openstreetmap.org/search?q=granada&format=json
+     output[0].osm_id = 344685
+     output[0].osm_type = 'relation' -> R (osmtype:  node (N), way (W) or relation (R) <- res[0].osm_type)
+     sample: https://nominatim.openstreetmap.org/details?osmtype=R&osmid=344685&format=json
+     output.admin_level
+
+ admin_level:
+ 4 -> ccaa / autonomous_communities
+ 6 -> provincia / province
+ 7 -> comarca / county
+ 8 -> municipio / ciudad | municipality / city
+ 9 -> distrito / district
+
+ comunidad autonoma, provincia, comarca, municipio / ciudad, distrito
+
+ https://wiki.openstreetmap.org/wiki/Tag:boundary%3Dadministrative
+ https://wiki.openstreetmap.org/wiki/Elements
+ https://wiki.openstreetmap.org/wiki/Map_Features#Boundary
+ https://wiki.openstreetmap.org/wiki/Boundaries#National
+*/
+
+
 module.exports = {
   arcgis : {
     name : 'arcgis',

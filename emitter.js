584815797d92c543dba347e45b7b0a496a0cdc50
Jorge Barrachina <nusquema@gmail.com>
2019-05-21 15:19:16 +0200

    Troubleshooting with the pipeline.Now it seems to work properly

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@ const WORDS_TO_TRACK = "PP,PSOE,CIUDADANOS,VOX,PODEMOS";
 
 const electionsStream = require('./lib/elections_stream.js')({
   ws : ws,
-  geocoders : ["arcgis"],
+  geocoders : ["arcgis", "osm"],
   twitter : {
     track : process.argv[2] || WORDS_TO_TRACK,
     credentials : require('./config/twitter_credentials.json')

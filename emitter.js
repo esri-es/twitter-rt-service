4039698bb4815db153d4bebdcc1d5eaf2e004ffb
RaulJimenezOrtega <hhkaos@gmail.com>
2019-05-24 11:12:27 +0200

    Chaging tracking keywords

--- emitter.js
+++ emitter.js
@@ -1,10 +1,10 @@
 let {ws,geocoders} = require('./config/elections.json');
 
-const WORDS_TO_TRACK = "PP,PSOE,CIUDADANOS,VOX,PODEMOS";
+const WORDS_TO_TRACK = "#PP,#PSOE,#CIUDADANOS,#VOX,#PODEMOS,#26M";
 
 const electionsStream = require('./lib/elections_stream.js')({
   ws : ws,
-  geocoders : ["arcgis", "osm"],
+  geocoders : ["arcgis"], //,"osm"],
   twitter : {
     track : process.argv[2] || WORDS_TO_TRACK,
     credentials : require('./config/twitter_credentials.json')
